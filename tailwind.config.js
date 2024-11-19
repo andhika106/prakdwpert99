629bf2892a1e1973c70b9a387d9bac452b7872be
andhika106 <152710227+andhika106@users.noreply.github.com>
2024-11-19 17:52:18 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.html"], 
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0D9488', 
+        secondary: '#F87171', 
+        accent: '#A78BFA', 
+        neutral: '#374151', 
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], 
+        display: ['Merriweather', 'serif'], 
+      },
+    },
+  },
+  plugins: [],
+}
